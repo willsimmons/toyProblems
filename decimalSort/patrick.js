a06b566e34f0fa7b11d340a2940ed5e484d6d64f
J. Patrick Sunday <j.patrick.sunday@gmail.com>
2018-05-05 12:55:39 -0700

    use same random number range as original prompt

--- decimalSort/patrick.js
+++ decimalSort/patrick.js
@@ -1,16 +1,18 @@
 // Sort nums using only the fractional portion of each number.
 // So under this ordering 50.40 is bigger than 100.09
 
-function generateRandomNumbers() {
+function generateRandomDecimalNumbers() {
   var numbers = [];
   for (let i = 0; i < 100; i++) {
-    const integerPart = Math.floor(Math.random() * 10000);
-    const fractionalPart = Math.floor(Math.random() * 10000) / 10000;
+    const integerPart = Math.floor(Math.random() * 1000);
+    const fractionalPart = Math.floor(Math.random() * 1000) / 1000;
     numbers.push(integerPart + fractionalPart);
   }
   return numbers;
 }
-const unsortedNumbers = generateRandomNumbers();
+
+const unsortedNumbers = generateRandomDecimalNumbers();
+
 const sortedNumbers = unsortedNumbers.slice().sort((a, b) => {
   const fractionalPartA = a % 1;
   const fractionalPartB = b % 1;
